c4ec44c4a9d51da47f72e62c697c093fbbc3a1be
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2019-01-12 14:46:15 -0600

    Create WDV341Intro.php
    Link to other pages

---
+++ WDV341Intro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>WDV341 Intro to PHP</title>
+		<meta charset="UTF-8">
+		<meta name="description" content="Home Page for WDV341">
+		<meta name="keywords" content="WDV341, html, css, php, programming, computer">
+		<link rel="stylesheet" type="text/css" href="../css/my_webpage.css">
+		<!-- 	Christina Buchwald
+			Date: Jan 12, 2019
+			WDV341 Home Page	-->
+	</head>
+	<body>
+		<h1 class="centerheader"><img src="../images/website_header.jpg" width="1240px" height="260px"/></h1>
+		<nav>
+			<ul>
+				<li>
+					<a href="../index.htm"> Home </a>
+				</li>
+				<li>
+					<a href="../WDV101/WDV101Intro.htm"> Homework for WDV101 </a>
+				</li>
+				<li>
+					<a href="../WDV221/WDV221Intro.htm"> Homework for WDV221 </a>
+				</li>
+				<li>
+					<a href="../WDV131/WDV131Intro.htm"> Homework for WDV131 </a>
+				</li>
+				<li>
+					<a href="../WDV341/WDV341Intro.php"> Homework for WDV341 </a>
+				</li>
+				<li>
+					<a href="../artwork.htm"> Artwork </a>
+				</li>
+			</ul>
+		</nav>
+		<div>
+			<h2>Homework Links WDV341</h2>
+		</div>
+	</body>
+</html>
