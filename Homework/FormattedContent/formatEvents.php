40b21cd4476cd4c3480feb25b34f55fdd5160e63
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2019-03-27 18:29:31 -0500

    Downloaded and unzipped code

---
+++ Homework/FormattedContent/formatEvents.php
@@ -0,0 +1,61 @@
+<?php
+	//Get the Event data from the server.
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>WDV341 Intro PHP  - Display Events Example</title>
+    <style>
+		.eventBlock{
+			width:500px;
+			margin-left:auto;
+			margin-right:auto;
+			background-color:#CCC;	
+		}
+		
+		.displayEvent{
+			text_align:left;
+			font-size:18px;	
+		}
+		
+		.displayDescription {
+			margin-left:100px;
+		}
+	</style>
+</head>
+
+<body>
+    <h1>WDV341 Intro PHP</h1>
+    <h2>Example Code - Display Events as formatted output blocks</h2>   
+    <h3>??? Events are available today.</h3>
+
+<?php
+	//Display each row as formatted output in the div below
+?>
+	<p>
+        <div class="eventBlock">	
+            <div>
+            	<span class="displayEvent">Event:</span>
+                <span>Presenter:</span>
+            </div>
+            <div>
+            	<span class="displayDescription">Description:</span>
+            </div>
+            <div>
+            	<span class="displayTime">Time:</span>
+            </div>
+            <div>
+            	<span class="displayDate">Date:</span>
+            </div>
+        </div>
+    </p>
+
+<?php
+	//Close the database connection	
+?>
+</div>	
+</body>
+</html>
